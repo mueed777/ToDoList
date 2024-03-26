cf7c5865c8981b517fddebd08038a1e6dbfcba4f
Abdul <abdul.hassan1@alaskaair.com>
2024-03-26 14:37:54 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Abdul Hassan on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
